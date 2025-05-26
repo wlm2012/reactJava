33408a7febfb9e4f875fc3eab1dc3fe9abc407ed
wlm2012 <541253785@qq.com>
2025-05-26 22:49:43 +0800

    调整类名

--- src/main/java/com/study/reactJava/adapter/scheduled/PushScheduled.java
+++ src/main/java/com/study/reactJava/adapter/scheduled/PushScheduled.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class PushScheduledServiceImpl {
+public class PushScheduled {
 
     private final Map<String, PushService> pushServiceMap;
 
